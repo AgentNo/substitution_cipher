ffdfbc080ee11f180697b9db76e9ebd663a1a016
AgentNo <dfaulkner2010@hotmail.co.uk>
2021-06-05 20:29:07 +0100

    Fixed cin looping issue

--- substitution_cipher/SubstitutionCipher.cpp
+++ substitution_cipher/SubstitutionCipher.cpp
@@ -3,9 +3,8 @@
 
 using namespace std;
 
+// Encode the user's message with the key via substitution
 string encode(string plaintext, string alphabet, string key) {
-    // Encode the user's message with the key - both the key and alphabet strings are of the same length, which allows for 1-1 mapping.
-    // Loop through the plaintext string and, for each character, read the corresponding letter in the key and add that to a new string
     string ciphertext;
 
     for (char c : plaintext) {
@@ -21,6 +20,7 @@ string encode(string plaintext, string alphabet, string key) {
     return ciphertext;
 }
 
+// Decode the user's message with the key via substitution
 string decode(string ciphertext, string alphabet, string key) {
     string plaintext;
     for (char c : ciphertext) {
@@ -36,16 +36,24 @@ string decode(string ciphertext, string alphabet, string key) {
 }
 
 int main() {
-    // Alphabet and key strings
     string alphabet{"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"};
     string key{"XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgghqdyvtkfuompciasr0h3lz8nql3"};
     bool loop = true;
 
     while (loop == true) {
-        // Get the user's initial input
-        cout << "\nWelcome to the super secret enconder/decoder!\nEnter an option: \n Enter 1 to encode\n Enter 2 to decode\n Enter 3 to exit.\n\n >>> ";
+        // Get the user's input
+        cout << "\nSubstitution Cipher\n\nEnter 1 to encode a message\nEnter 2 to decode a message\nEnter 3 to exit.\n\n>>> ";
         int option;
         cin >> option;
+
+        // If cin gets unexpected input, it can enter into the fail state and cause an infinite loop. To fix this, 
+        // cin must be cleared and any bad characters ignored to force it to accept input again.
+        if (cin.fail()) {
+            cout << "Please enter an integer";
+            cin.clear();
+            // discard 'bad' character(s)
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
 
         switch(option) {
             case 1:
@@ -56,7 +64,7 @@ int main() {
                     cin.ignore();
                     getline(cin, plaintext);
                     plaintext = encode(plaintext, alphabet, key);
-                    cout << "\nEncoding...\n\nThe ciphertext is " + plaintext + "\n";
+                    cout << "\n\nThe ciphertext is " + plaintext + "\n";
                     system("pause");
                 }
                 break;
@@ -68,7 +76,7 @@ int main() {
                     cin.ignore();
                     getline(cin, ciphertext);
                     ciphertext = decode(ciphertext, alphabet, key);
-                    cout << "\nDecoding...\n\nThe plaintext is " + ciphertext + "\n";
+                    cout << "\n\nThe plaintext is " + ciphertext + "\n";
                     system("pause");
                 }
                 break;
@@ -80,6 +88,7 @@ int main() {
             default:
                 // Error - unrecognised input
                 cout << "\nError - invalid input. Please enter 1, 2 or 3.\n";
+                break;
         }
     }
     // Exit the application
